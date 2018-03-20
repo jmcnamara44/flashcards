132db6922d4bd9c105a1886f5920b5ae182e8988
Azamat Bekmuratov and Jimmy McNamara <jimmy@>
2018-03-20 15:32:03 -0700

    add bootstrap features separating flashcards into 3 columns, making flashcards look like buttons, and complete scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,75 @@ $(document).ready(function() {
   });
 
   $("#operator").click(function() {
-
     $("#def-operator").toggle();
   });
+
+  $("#variable").click(function() {
+    $("#def-variable").toggle();
+  });
+
+  $("#var-nam-conv").click(function() {
+    $("#def-var-nam-conv").toggle();
+  });
+
+  $("#function").click(function() {
+    $("#def-function").toggle();
+  });
+
+  $("#method").click(function() {
+    $("#def-method").toggle();
+  });
+
+  $("#argument").click(function() {
+    $("#def-argument").toggle();
+  });
+
+  $("#parameter").click(function() {
+    $("#def-parameter").toggle();
+  });
+
+  $("#return").click(function() {
+    $("#def-return").toggle();
+  });
+
+  $("#chain-method").click(function() {
+    $("#def-chain-method").toggle();
+  });
+
+  $("#string").click(function() {
+    $("#def-string").toggle();
+  });
+
+  $("#boolean").click(function() {
+    $("#def-boolean").toggle();
+  });
+
+  $("#undefined").click(function() {
+    $("#def-undefined").toggle();
+  });
+
+  $("#nan").click(function() {
+    $("#def-nan").toggle();
+  });
+
+  $("#escape").click(function() {
+    $("#def-escape").toggle();
+  });
+
+  $("#alert").click(function() {
+    $("#def-alert").toggle();
+  });
+
+  $("#comment").click(function() {
+    $("#def-comment").toggle();
+  });
+
+  $("#jquery").click(function() {
+    $("#def-jquery").toggle();
+  });
+
+  $("#attribute").click(function() {
+    $("#def-attribute").toggle();
+  });
+
 });
